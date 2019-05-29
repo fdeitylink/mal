e07b375c4e8ab501043cb35639039a02cb70b910
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-05-29 17:41:45 -0400

    Step 9: Try

--- clojure/src/mal/core.clj
+++ clojure/src/mal/core.clj
@@ -3,7 +3,17 @@
             [clojure.string :as s]))
 
 (def core-ns
-  {'+ +
+  {'nil? nil?
+   'true? true?
+   'false? false?
+
+   'symbol symbol
+   'symbol? symbol?
+
+   'keyword keyword
+   'keyword? keyword?
+
+   '+ +
    '- -
    '* *
    '/ #(int (apply / %&))
@@ -16,20 +26,38 @@
 
    'list list
    'list? seq?
+
+   'vector vector
+   'vector? vector?
+
+   'sequential? sequential?
+
+   'hash-map hash-map
+   'map? map?
+
    'empty? empty?
    'count count
    'cons cons
    'concat concat
+   'assoc assoc
+   'dissoc dissoc
    'nth nth
+   'get get
    'first first
    'rest rest
+   'keys #(apply list (keys %))
+   'vals #(apply list (vals %))
+
+   'map #(apply list (map %1 %2))
+
+   'apply apply
+
+   'read-string mal.reader/read-str
 
    'pr-str #(s/join " " (map mal.printer/pr-str %&))
    'str #(apply str (map (fn [form] (mal.printer/pr-str form false)) %&))
    'prn #(println (s/join " " (map mal.printer/pr-str %&)))
    'println #(println (s/join " " (map (fn [form] (mal.printer/pr-str form false)) %&)))
-
-   'read-string mal.reader/read-str
 
    'slurp slurp
 
@@ -37,4 +65,6 @@
    'atom? (partial instance? clojure.lang.Atom)
    'deref deref
    'reset! reset!
-   'swap! swap!})
+   'swap! swap!
+
+   'throw #(throw (ex-info "mal exception" {:data %}))})
