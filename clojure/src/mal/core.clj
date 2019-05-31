99cb6622a3dda87257aa1dda24612abfc6ba0a4c
fdeitylink <11952260+fdeitylink@users.noreply.github.com>
2019-05-30 21:37:16 -0400

    Step A: Mal

--- clojure/src/mal/core.clj
+++ clojure/src/mal/core.clj
@@ -7,6 +7,9 @@
    'true? true?
    'false? false?
 
+   'string? string?
+   'number? number?
+
    'symbol symbol
    'symbol? symbol?
 
@@ -30,6 +33,8 @@
    'vector vector
    'vector? vector?
 
+   'seq #(seq (if (string? %) (map str %) %))
+
    'sequential? sequential?
 
    'hash-map hash-map
@@ -38,6 +43,7 @@
    'empty? empty?
    'count count
    'cons cons
+   'conj conj
    'concat concat
    'assoc assoc
    'dissoc dissoc
@@ -51,16 +57,22 @@
 
    'map #(apply list (map %1 %2))
 
+   'fn? #(and (fn? %) (not (:is-macro (meta %))))
+   'macro? #(and (fn? %) (:is-macro (meta %)))
+
    'apply apply
 
    'read-string mal.reader/read-str
+
+   'meta #(:mal-meta (meta %))
+   'with-meta #(with-meta %1 (assoc (meta %1) :mal-meta %2))
 
    'pr-str #(s/join " " (map mal.printer/pr-str %&))
    'str #(apply str (map (fn [form] (mal.printer/pr-str form false)) %&))
    'prn #(println (s/join " " (map mal.printer/pr-str %&)))
    'println #(println (s/join " " (map (fn [form] (mal.printer/pr-str form false)) %&)))
 
-   'slurp slurp
+   'throw #(throw (ex-info "mal exception" {:data %}))
 
    'atom atom
    'atom? (partial instance? clojure.lang.Atom)
@@ -68,4 +80,8 @@
    'reset! reset!
    'swap! swap!
 
-   'throw #(throw (ex-info "mal exception" {:data %}))})
+   'slurp slurp
+
+   'readline read-line
+
+   'time-ms #(System/currentTimeMillis)})
